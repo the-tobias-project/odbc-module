300b326cab3f5ac4945566e49b659573507d2ef9
leandroroser <learoser@gmail.com>
2023-01-25 17:13:07 -0300

    add

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 
 module use --append $PWD/software/modules/contribs
 echo "DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.Renviron
-source ~/.bashrc
 echo "Databricks module installed succesfully!"
