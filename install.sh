3a47b5f6ef2d17569ffa92f370a0f80e172e5643
leandroroser <learoser@gmail.com>
2023-01-25 17:06:14 -0300

    add

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 module use --append $PWD/software/modules/contribs
 echo "export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.bashrc
