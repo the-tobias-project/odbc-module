931039987a8f7c5ebd4f83109d66537899f18145
leandroroser <learoser@gmail.com>
2023-01-25 16:47:26 -0300

    add install

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 cd lua-modules 
 module use --append $PWD/software/modules/contribs
-export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar
+echo "export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.bashrc
+source ~/.bashrc
