45a4592b3ac58043554e4b0cd311d25a77164150
leandroroser <learoser@gmail.com>
2023-02-08 03:08:18 -0300

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash get_databricks_driver.sh
+export THIS_LOCATION=$PWD/software/user/open/databricks-odbc/4.2.0
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "$HOME/.odbc.ini"
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "$HOME/.odbcinst.ini"
 module use --append "$PWD/software/modules/contribs"
