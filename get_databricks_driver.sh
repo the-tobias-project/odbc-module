d80d4a51e39f5b6fda4892a619cbef741d3bff41
leandroroser <learoser@gmail.com>
2023-02-08 05:33:33 -0300

    add

--- get_databricks_driver.sh
+++ get_databricks_driver.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir driver
 cd driver
-curl -0 https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.29/SimbaSparkODBC-2.6.29.1049-LinuxRPM-64bit.zip
+wget https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.29/SimbaSparkODBC-2.6.29.1049-LinuxRPM-64bit.zip
 echo "ce2b0e5b7f437a448cec784e2c79907b886e7cb28202d0c9d1733511b488aca2  SimbaSparkODBC-2.6.29.1049-LinuxRPM-64bit.zip" > shasum
 sha256sum -c shasum | tee >(grep "FAILED" && exit 1)
 unzip SimbaSparkODBC-2.6.29.1049-LinuxRPM-64bit.zip
@@ -18,7 +18,7 @@ rm -rf driver
 
 mkdir driver
 cd driver
-curl -O https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.32/DatabricksJDBC42-2.6.32.1054.zip
+wget https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.32/DatabricksJDBC42-2.6.32.1054.zip
 echo "354bbf2ae6677779b581c2c410558e65ab4b8815c0e51cd317c5b909fd6ad416  DatabricksJDBC42-2.6.32.1054.zip" > shasum
 sha256sum -c shasum | tee >(grep "FAILED" && exit 1)
 unzip DatabricksJDBC42-2.6.32.1054.zip
