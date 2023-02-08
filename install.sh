c04100a929f760a5ceb4af151a29aa378aa114d8
leandroroser <learoser@gmail.com>
2023-02-08 08:20:52 -0300

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash get_databricks_driver.sh
+
 export THIS_LOCATION=$PWD/software/user/open/databricks-odbc/4.2.0
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "$HOME/.odbc.ini"
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "$HOME/.odbcinst.ini"
@@ -10,4 +11,21 @@ module use --append "$PWD/software/modules/contribs"
 echo "export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.Renviron
 echo "export ODBCSYSINI=$HOME" >> ~/.bashrc
 echo "export ODBCINI=$HOME/.odbc.ini" >> ~/.bashrc
+
 echo "Databricks modules succesfully installed!"
+
+echo "Running direnv setup..."
+curl -sfL https://direnv.net/install.sh | bash
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+mkdir -p  ~/.config/direnv
+touch  ~/.config/direnv/direnv.toml
+
+
+echo "# [DATABRICKS SETTINGS]\n
+DATABRICKS_HOSTNAME=\n
+DATABRICKS_TOKEN=\n
+DATABRICKS_HTTP_PATH=\n"
+
+echo "Done"
+echo "Pease fill in the .env file in $HOME/.env with the Databricks variables and then run: direnv allow"
+
