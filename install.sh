427ed85f39fadcfcb16b02c637bd093dedbb30fe
leandroroser <learoser@gmail.com>
2023-02-08 07:03:19 -0300

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "$HOM
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "$HOME/.odbcinst.ini"
 module use --append "$PWD/software/modules/contribs"
 echo "export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.Renviron
-
 echo "export ODBCSYSINI=$HOME" >> ~/.bashrc
 echo "export ODBCINI=$HOME/.odbc.ini" >> ~/.bashrc
 echo "Databricks modules succesfully installed!"
