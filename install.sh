2296711d3b5ab73345b33d5e6d749d8326491d21
leandroroser <learoser@gmail.com>
2023-02-08 07:44:26 -0300

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ bash get_databricks_driver.sh
 export THIS_LOCATION=$PWD/software/user/open/databricks-odbc/4.2.0
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "$HOME/.odbc.ini"
 envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "$HOME/.odbcinst.ini"
-envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/simba/spark/lib/64/simba.sparkodbc.ini" > "$PWD/software/user/open/databricks-odbc/4.2.0/simba/spark/lib/64/simba.sparkodbc.ini"
+envsubst < "$PWD/software/user/open/databricks-odbc/4.2.0/simba/spark/lib/64/simba.sparkodbc.ini" > temporal.txt
+mv temporal.txt "$PWD/software/user/open/databricks-odbc/4.2.0/simba/spark/lib/64/simba.sparkodbc.ini"
 module use --append "$PWD/software/modules/contribs"
 echo "export DATABRICKS_JAR=$PWD/software/user/open/databricks-jdbc/4.2.0/lib/DatabricksJDBC42.jar" >> ~/.Renviron
 echo "export ODBCSYSINI=$HOME" >> ~/.bashrc
