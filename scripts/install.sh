450df93753f0543fed62bdc715d4e38b9f400e43
leandroroser <learoser@gmail.com>
2023-02-09 22:13:46 -0300

    add

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,7 @@ module unload R
 eval "$(direnv hook bash)"
 direnv allow
 
+### install unixODBC
 
 md5sum -c unixODBC-2.3.11.tar.gz.md5 | tee /dev/tty | grep "FAILED" && echo "MD5SUM DOES NOT MATCH FOR uninxODBC" && exit 1
 tar xvf unixODBC-2.3.11.tar.gz
@@ -17,6 +18,8 @@ cd unixODBC-2.3.11/
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${THISPATH}/driver/unixODBC-2.3.11/DriverManager/.libs" >> ~/.env
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${THISPATH}/driver/unixODBC-2.3.11/odbcinst/.libs" >> ~/.env
 cd $THISPATH/driver
+
+### Install SimbaSpark
 
 echo "ce2b0e5b7f437a448cec784e2c79907b886e7cb28202d0c9d1733511b488aca2  SimbaSparkODBC-2.6.29.1049-LinuxRPM-64bit.zip" > shasum
 sha256sum -c shasum | tee /dev/tty | grep "FAILED" && echo "MD5SUM DOES NOT MATCH FOR SimbaSparkODBC" && exit 1
@@ -32,6 +35,20 @@ envsubst < "$THISPATH/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini
 envsubst < "$SPARKPATH/lib/64/simba.sparkodbc.ini" > temporal.txt
 mv temporal.txt "$SPARKPATH/lib/64/simba.sparkodbc.ini"
 module use --append "$THISPATH/software/modules/contribs"
+
+### install R package 
+
+module load R/4.2.0
+
+R --vanilla <<EOF
+install.packages(c('DBI', 'odbc'), repos='http://cran.us.r-project.org')
+q()
+EOF
+
+git clone https://github.com/the-tobias-project/loaddatabricks
+R CMD build loaddatabricks 
+R CMD INSTALL loaddatabricks*.tar.gz
+rm -rf loaddatabricks*
 
 
 echo "Databricks modules succesfully installed!"
