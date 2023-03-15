341e3aff3c341c7b54794da70642a0fe88647bd6
leandroroser <learoser@gmail.com>
2023-03-15 05:49:53 -0300

    fix

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,3 +1,3 @@
 libsdir <- Sys.getenv("R_LIBS_USER")
 dir.create(libsdir, showWarnings = FALSE, recursive=TRUE) 
-install.packages(c('DBI', 'odbc', 'dotenv'), repos='http://cran.us.r-project.org', lib=libsdir, dependencies = TRUE)
+install.packages(c('DBI', 'odbc', 'dotenv'), repos='http://cran.us.r-project.org', lib=libsdir)
