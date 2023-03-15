0de02831f620cb2fcf5134056d89b31e552dd471
leandroroser <learoser@gmail.com>
2023-03-15 01:42:27 -0300

    update devtoolset

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,4 +1,3 @@
 libsdir <- Sys.getenv("R_LIBS_USER")
 dir.create(libsdir, showWarnings = FALSE, recursive=TRUE) 
-install.packages('odbc', repos='http://cran.us.r-project.org', lib=libsdir, type="source")
-install.packages(c('DBI', 'dotenv'), repos='http://cran.us.r-project.org', lib=libsdir)
+install.packages(c('DBI', 'odbc', 'dotenv'), repos='http://cran.us.r-project.org', lib=libsdir, dependencies = TRUE)
