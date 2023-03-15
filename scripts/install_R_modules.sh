4a1f46bf446c6d188695723b69994564ef9cd029
leandroroser <learoser@gmail.com>
2023-03-14 22:05:22 -0300

    update devtoolset

--- scripts/install_R_modules.sh
+++ scripts/install_R_modules.sh
@@ -11,10 +11,10 @@ module load R/4.2.0
 module load unixodbc/2.3.9
 
 R --vanilla <<EOF
-dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive=TRUE)
-.libPaths(Sys.getenv("R_LIBS_USER"))  
-install.packages(c('DBI', 'dotenv'), repos='http://cran.us.r-project.org', lib=Sys.getenv("R_LIBS_USER"))
-install.packages('odbc', repos='http://cran.us.r-project.org', lib=Sys.getenv("R_LIBS_USER"), type="source")
+libsdir <- Sys.getenv("R_LIBS_USER")
+dir.create(libsdir, showWarnings = FALSE, recursive=TRUE) 
+install.packages('odbc', repos='http://cran.us.r-project.org', lib=libsdir, type="source")
+install.packages(c('DBI', 'dotenv'), repos='http://cran.us.r-project.org', lib=libsdir)
 EOF
 
 git clone https://github.com/the-tobias-project/loaddatabricks
