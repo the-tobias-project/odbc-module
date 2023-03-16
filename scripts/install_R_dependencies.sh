25d35099258fcaa4abf2cc97d7c78d0bbc16baa4
leandroroser <learoser@gmail.com>
2023-03-16 18:16:28 -0300

    use binaries

--- scripts/install_R_dependencies.sh
+++ scripts/install_R_dependencies.sh
@@ -11,10 +11,9 @@ module unload R
 module load R/4.2.0
 module load unixodbc/2.3.9
 
-declare -a packages=("odbc_1.3.4" "DBI_1.1.3" "dotenv_1.0" "rlang_1.1.0")
-for package in for i in "${packages[@]}";do
-    curl -L https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/${package}.tar.gz?r_version=4.2  | tar xvz -C ${R_LIBS_USER}
-done
+while read -r package; do
+    curl -L "https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/${package}.tar.gz?r_version=4.2" | tar xvz -C "${R_LIBS_USER}"
+done < R_packages.txt
 
 #Rscript ${THISPATH}/scripts/dependencies.R
 
