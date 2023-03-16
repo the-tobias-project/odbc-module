a01246a5ee4e1bf3604ab94613c948db9196071c
leandroroser <learoser@gmail.com>
2023-03-16 08:14:57 -0300

    use binaries

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export THISPATH=$1/odbc-module
+export THISPATH=$1
 
 envsubst < "${THISPATH}/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "${HOME}/.odbc.ini"
 envsubst < "${THISPATH}/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "${HOME}/.odbcinst.ini"
