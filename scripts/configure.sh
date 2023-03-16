24fbcd624a0b8faf56dd14bc3d10b94ea2067863
leandroroser <learoser@gmail.com>
2023-03-16 15:01:08 -0300

    use binaries

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -20,7 +20,7 @@ cat >> "${HOME}/.env" <<EOF
 MODULE_FOLDER=${THISPATH}
 ODBCINI=${HOME}/.odbc.ini
 ODBCSYSINI=${HOME}
-MODULEPATH=${MODULEPATH}:${THISPATH}/software/modules/
+MODULEPATH=${MODULEPATH}:${THISPATH}/software/modules/contribs
 R_LIBS_USER=${THISPATH}/R/x86_64-pc-linux-gnu-library/4.2
 SPARKPATH=${THISPATH}/software/user/open/databricks-odbc/4.2.0/simba/spark
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISPATH}/driver/unixODBC-2.3.11/DriverManager/.libs:${THISPATH}/driver/unixODBC-2.3.11/odbcinst/.libs
@@ -28,5 +28,12 @@ EOF
 
 
 echo -e "\n#ODBC CONFIGURATION>>>>\nexport \$(grep -v '^#' ${HOME}/.env | xargs)\n#<<<<ODBC CONFIGURATION" >> "${HOME}/.bashrc"
-echo "Done. Please fill in the .env file at ${HOME}/.env with the Databricks variables"
+
+echo -e "\n\n-----------------------------------------------------------------------"
+echo -e "Done. This is the resulting configuration at ${HOME}/.env, check that it is correct:"
+cat "${HOME}/.env"
+echo -e "\n\nThe following lines were aded to ${HOME}/.bashrc:"
+echo -e "\n#ODBC CONFIGURATION>>>>\nexport \$(grep -v '^#' ${HOME}/.env | xargs)\n#<<<<ODBC CONFIGURATION" 
+echo -e "\n-----------------------------------------------------------------------\n\n"
+echo "Please fill in the .env file at ${HOME}/.env with the Databricks variables"
 
