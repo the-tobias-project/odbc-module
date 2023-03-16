17530ae86a4c4c5cead1019d2f5e2a037c4c8936
leandroroser <learoser@gmail.com>
2023-03-16 18:11:41 -0300

    use binaries

--- scripts/install_R_dependencies.sh
+++ scripts/install_R_dependencies.sh
@@ -11,10 +11,11 @@ module unload R
 module load R/4.2.0
 module load unixodbc/2.3.9
 
-curl -L https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/odbc_1.3.4.tar.gz?r_version=4.2  | tar xvz -C ${R_LIBS_USER}
-curl -L https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/DBI_1.1.3.tar.gz?r_version=4.2 | tar xvz -C ${R_LIBS_USER}
-curl -L https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/dotenv_1.0.3.tar.gz?r_version=4.2 | tar xvz -C ${R_LIBS_USER}
-curl -L  https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/rlang_1.1.0.tar.gz?r_version=4.2 | tar xvz -C ${R_LIBS_USER}
+declare -a packages=("odbc_1.3.4" "DBI_1.1.3" "dotenv_1.0" "rlang_1.1.0")
+for package in for i in "${packages[@]}";do
+    curl -L https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/${package}.tar.gz?r_version=4.2  | tar xvz -C ${R_LIBS_USER}
+done
+
 #Rscript ${THISPATH}/scripts/dependencies.R
 
 git clone https://github.com/the-tobias-project/loaddatabricks
