f2c080ffd93857adb7686ec3b87621e92edcd98d
leandroroser <learoser@gmail.com>
2023-03-16 18:52:35 -0300

    use binaries

--- scripts/install_R_dependencies.sh
+++ scripts/install_R_dependencies.sh
@@ -3,8 +3,10 @@
 set -e
 
 THISPATH=$1
-export R_LIBS_USER=${THISPATH}/R/x86_64-pc-linux-gnu-library/4.2
+REPO=https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib
+R_VERSION=4.2
 
+export R_LIBS_USER=${THISPATH}/R/x86_64-pc-linux-gnu-library/4.2
 mkdir -p ${R_LIBS_USER}
 
 module unload R
@@ -12,7 +14,7 @@ module load R/4.2.0
 module load unixodbc/2.3.9
 
 while read -r package; do
-    curl -L "https://packagemanager.rstudio.com/cran/__linux__/centos7/latest/src/contrib/${package}.tar.gz?r_version=4.2" | tar xvz -C "${R_LIBS_USER}"
+    curl -L "${REPO}/${package}.tar.gz?r_version=${R_VERSION}" | tar xvz -C "${R_LIBS_USER}"
 done < ${THISPATH}/scripts/R_packages.txt
 
 #Rscript ${THISPATH}/scripts/dependencies.R
