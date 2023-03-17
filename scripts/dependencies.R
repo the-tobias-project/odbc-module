9663340d869dba1d4609f5e6f4ccc639aa4ce82d
leandroroser <learoser@gmail.com>
2023-03-17 01:42:16 -0300

    authorization

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,4 +1,5 @@
 libsdir <- Sys.getenv("R_LIBS_USER")
+Sys.setenv(TZ="America/Los_Angeles")
 #dir.create(libsdir, showWarnings = FALSE, recursive=TRUE) 
 package_manager <- "https://cloud.r-project.org/"
-install.packages(c('DBI', 'dotenv'), repos=package_manager, lib=libsdir)
+install.packages(c('DBI', 'dotenv'), repos=package_manager, lib=libsdir, dependencies=TRUE)
