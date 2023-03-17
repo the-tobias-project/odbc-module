c292ccda93f1b3f417c52bee6dccb98a98988f1a
leandroroser <learoser@gmail.com>
2023-03-17 01:34:00 -0300

    authorization

--- scripts/install_R_dependencies.sh
+++ scripts/install_R_dependencies.sh
@@ -6,6 +6,7 @@ THISPATH=$1
 DISTRO=centos7
 R_VERSION=4.2
 REPO=https://packagemanager.rstudio.com/cran/__linux__/${DISTRO}/latest/src/contrib
+package=odbc_1.3.4
 
 export R_LIBS_USER=${THISPATH}/R/x86_64-pc-linux-gnu-library/4.2
 mkdir -p ${R_LIBS_USER}
@@ -14,10 +15,7 @@ module unload R
 module load R/4.2.0
 module load unixodbc/2.3.9
 
-while read -r package; do
-    curl -L "${REPO}/${package}.tar.gz?r_version=${R_VERSION}" | tar xvz -C "${R_LIBS_USER}"
-done < ${THISPATH}/scripts/R_requirements.txt
-
+curl -L "${REPO}/${package}.tar.gz?r_version=${R_VERSION}" | tar xvz -C "${R_LIBS_USER}"
 Rscript ${THISPATH}/scripts/dependencies.R
 
 git clone https://github.com/the-tobias-project/loaddatabricks
