bfbd1a4ae6661a807b4d38c21c9088ff9ad79249
leandroroser <learoser@gmail.com>
2023-03-21 02:01:36 -0300

    add install script

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ while true; do
     echo -e "\nSelect an install option:"
     echo -e "${YELLOW}1) Personal${NC}"
     echo -e "${YELLOW}2) Group${NC}"
+    
     printf "Your option: "
-
     read -r option </dev/tty
+    
     set +e
     case $option in
         1)
