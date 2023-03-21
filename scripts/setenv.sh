d019f72aa35afde1ebf97f406daac2b90646424d
leandroroser <learoser@gmail.com>
2023-03-21 01:47:35 -0300

    add install script

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e pipefail
-
 export THISPATH=$1
+
+set -e pipefail
 
 envsubst < "${THISPATH}/software/user/open/databricks-odbc/4.2.0/conf/odbc.ini" > "${HOME}/.odbc.ini"
 envsubst < "${THISPATH}/software/user/open/databricks-odbc/4.2.0/conf/odbcinst.ini" > "${HOME}/.odbcinst.ini"
