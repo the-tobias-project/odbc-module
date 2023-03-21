04ff00778b5ebdc3a41788ca5229f46a31266260
leandroroser <learoser@gmail.com>
2023-03-20 21:33:02 -0300

    add install script

--- scripts/authorize.sh
+++ scripts/authorize.sh
@@ -9,7 +9,7 @@ echo -e "Authorizing...\n"
 
 which az >/dev/null || echo Azure CLI not found. Please go to https://learn.microsoft.com/en-us/cli/azure/install-azure-cli and install the appropriate version
 
-az login --use-device-code --allow-no-subscriptions
+az login --use-device-code --allow-no-subscriptions >/dev/null
 
 # Retrieve the AAD token from Azure
 export DATABRICKS_AAD_TOKEN=$(jq .accessToken -r <<< `az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d`)
@@ -18,7 +18,7 @@ dbhost=https://$(grep "Host=" ~/.odbc.ini | cut -d "=" -f 2 | awk '{print $1}')
 
 echo "Authorizing: $dbhost"
 
-databricks configure --aad-token --host $dbhost
+databricks configure --aad-token --host $dbhost >/dev/null
 
 DATABRICKS_PAT=$(jq .token_value -r <<< `databricks tokens create --lifetime-seconds 600 --comment "Personal Access Token"`)
 
